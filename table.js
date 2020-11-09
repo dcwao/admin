88a98215dc677a7c735c9296ea763b83070b3817
dcwao <61139815+dcwao@users.noreply.github.com>
2020-11-09 15:01:18 +0700

    Update table.js

--- table.js
+++ table.js
@@ -1,8 +1,4 @@
-$.ajax({
-    url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQoFDk4HZ8maYQDzvTD-34MjnxxFm-LzVVjR1uzI_ykHhjvNuioksekrtxROS0-D5ARwu8VtBBq7xH7/pub?gid=0&single=true&output=csv',
-    dataType: 'text',
-  }).done(successFunction);
-  function successFunction(data) {
+function successFunction(data) {
   var allRows = data.split(/\r?\n|\r/);
   var table = '<table>';
   for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
@@ -36,3 +32,7 @@ $.ajax({
   table += '</table>';
   $('body').append(table);
 }
+$.ajax({
+    url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQoFDk4HZ8maYQDzvTD-34MjnxxFm-LzVVjR1uzI_ykHhjvNuioksekrtxROS0-D5ARwu8VtBBq7xH7/pub?gid=0&single=true&output=csv',
+    dataType: 'text',
+  }).done(successFunction);
